45a93f946fa6f54e340195ab35f7412ac8f9073b
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-02-23 11:02:06 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", submitTask);
+
+  function submitTask(Event) {
+    Event.preventDefault();
+    let taskInput = document.querySelector("#new-task-description");
+    let listElement = document.querySelector("li");
+    let userInput = taskInput.value;
+    listElement.innerHTML = userInput;
+  }
+
+  function removeTask(event) {
+    listElement.remove();
+  }
+
+  span = document.createElement("span");
+  listElement.appendChild(span);
+  span.userInput = "[x]";
+  span.addEventListener("click", removeTask);
 });
